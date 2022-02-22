7d9b1ad7b55e6b1ebdd262dd49e48a6bf5988317
vinodneppalli62 <vinodneppalli62@gmail.com>
2022-02-22 00:59:46 -0500

    Changes for showing bookmarks folder wise when imported external file and also manual adding

--- js/bookmark.js
+++ js/bookmark.js
@@ -14,13 +14,15 @@
     function saveBookmark(event){
         var siteName = document.getElementById("siteName").value;
         var siteUrl = document.getElementById("siteUrl").value;
+        var siteFolder= document.getElementById("siteFolder").value;
         
         if(!validateBookmarkForm(siteName,siteUrl)){
             return false;
         }
         var bookmark = {
             name:siteName.charAt(0).toUpperCase() + siteName.slice(1),
-            url:siteUrl
+            url:siteUrl,
+            folder: siteFolder || "Other"
         };
     
         if(localStorage.getItem("bookmarks") === null){
@@ -28,7 +30,7 @@
         }else{
             var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
             var isBookmarkPresent = bookmarks.filter(function(bookmark){
-                return bookmark.url === siteUrl;
+                return bookmark.folder === siteFolder && bookmark.url === siteUrl;
             }).length > 0;
             if(!isBookmarkPresent){
                 bookmarks.push(bookmark);
@@ -45,25 +47,67 @@
     
     function fetchBookmarks(){
         var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
-        var bookmarksResults = document.getElementById("bookmarkResults");
-        bookmarksResults.innerHTML = '';
         if(bookmarks && bookmarks.length > 0){
-            for (let index = 0; index < bookmarks.length; index++) {
-                bookmarksResults.innerHTML +='<div class="bg-light">'+
-                                              '<label id="website-name">'+bookmarks[index].name+
-                                              ' <a id="view_'+index+'" href="'+bookmarks[index].url+'" title="'+bookmarks[index].name+', view bookmark" target="_blank" class="btn btn-success" style="float:left;margin-right:2px"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">'+
-                                              ' <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>'+
-                                              ' <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>'+
-                                              ' </svg></a> '+
-                                              ' <a id="delete_'+index+'" onclick="deleteBookmark(\''+bookmarks[index].url+'\')" title="'+bookmarks[index].name+', delete bookmark" href="#" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">'+
-                                              ' <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>'+
-                                              ' </svg></a> '+
-                                              '</label>'+
-                                              '</div>';
-                                             
-            }
+           prepareFolderDropdown(getGroupedListByFolder(bookmarks));
+           changeFolder("");
         }
     }
+
+    function prepareFolderDropdown(groupedBookmarks) {
+            var folderOptions = "<option value='' disabled selected>Folder</option>";
+            for (var key in groupedBookmarks) {
+                folderOptions += "<option value='"+key+"'>" + key+ "</option>";
+            }
+            document.getElementById("folder").innerHTML = folderOptions;
+    }
+
+    function changeFolder(value) {
+        var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+        var groupedBookmarks =getGroupedListByFolder(bookmarks);
+        if (value.length == 0) document.getElementById("siteResults").innerHTML = "<option value='' disabled selected>Sites</option>";
+        else {
+            var siteOptions = "<option value='' disabled selected>Sites</option>";
+            groupedBookmarks[value].forEach(function(option){
+                siteOptions += "<option>" + option.name + "</option>";
+            });
+            document.getElementById("siteResults").innerHTML = siteOptions;
+        }
+        var bookmarksResults = document.getElementById("bookmarkResults");
+        bookmarksResults.innerHTML = "";
+    }
+
+    function changeSiteDropdownOption(value) {
+        var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+        var bookmarksResults = document.getElementById("bookmarkResults");
+        bookmarksResults.innerHTML = ""
+        if (value.length == 0) return false;
+        else {
+              var bookmark = bookmarks.filter(function(item){
+                  return item.name === value;
+              });
+              bookmarksResults.innerHTML +='<div class="bg-light">'+
+                                              '<label id="website-name">'+bookmark[0].name+
+                                               ' <a id="view" href="'+bookmark[0].url+'" title="'+bookmark[0].name+', view bookmark" target="_blank" class="btn btn-success" style="float:left;margin-right:2px"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">'+
+                                               ' <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>'+
+                                               ' <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>'+
+                                               ' </svg></a> '+
+                                               ' <a id="delete" onclick="deleteBookmark(\''+bookmark[0].url+'\')" title="'+bookmarks[0].name+', delete bookmark" href="#" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">'+
+                                               ' <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>'+
+                                               ' </svg></a> '+
+                                               '</label>'+
+                                               '</div>';
+        }
+    }
+
+    function getGroupedListByFolder(bookmarks){
+        return bookmarks.reduce((group, bookmark) => {
+            var { folder } = bookmark;
+            group[folder] = group[folder] ?? [];
+            group[folder].push(bookmark);
+            return group;
+          }, {});
+    }
+
     
     function deleteBookmark(url){
         var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
@@ -96,7 +140,7 @@
     function exportToFile(event){
         event.preventDefault();
         var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
-        var arrayHeader = ["name","url"];
+        var arrayHeader = ["name","url","folder"];
         export_csv(arrayHeader,bookmarks,",","Bookmarks");
     }
     
@@ -175,14 +219,28 @@
 
     function htmlToArray(str){
         var htmlObject = $(str);
-        var headers = ["name","url"];
-        var links = htmlObject.contents().find( "a" );
-        var arr =  links.map(function (link) {
-            if(links[link].tagName === "A"){
-               return {"name":links[link].text,"url":links[link].href};
+        var folderNodes = htmlObject.contents().find("H3");
+        var allLinks = htmlObject.contents().find("A");
+        var arr = []
+        for(var nodeIndex = 0;nodeIndex < folderNodes.length;nodeIndex++){
+            if(folderNodes[nodeIndex].tagName === "H3"){
+                var folderName = folderNodes[nodeIndex].innerText;
+                var childNodes = folderNodes[nodeIndex].nextElementSibling.childNodes;
+               for(var i = 0;i<childNodes.length;i++){
+                   if(childNodes[i].firstChild.tagName === "A"){
+                    arr.push({"name":childNodes[i].firstChild.text,"url":childNodes[i].firstChild.href,"folder":folderName});
+                   }
+               }
             }
-        });
-        
+        }
+        for(var i = 0; i<allLinks.length;i++){
+            var isNotPresent = arr.filter(function(item){
+                return item.url === allLinks[i].href;
+            }).length === 0;
+            if(isNotPresent){
+                arr.push({"name":allLinks[i].text,"url":allLinks[i].href,"folder":"Other Bookmarks"});
+            }
+        }
         return arr;
     }
 
